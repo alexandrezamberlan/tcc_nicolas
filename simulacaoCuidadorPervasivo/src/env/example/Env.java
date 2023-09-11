8241146fa1bd0c959f4fa953d6e976908e574d19
Alexandre Zamberlan <alexz@ufn.edu.br>
2023-09-11 18:21:16 -0300

    geradores ação

--- simulacaoCuidadorPervasivo/src/env/example/Env.java
+++ simulacaoCuidadorPervasivo/src/env/example/Env.java
@@ -6,20 +6,56 @@ import jason.asSyntax.*;
 import jason.environment.*;
 import jason.asSyntax.parser.*;
 
+import java.util.Random;
 import java.util.logging.*;
 
 public class Env extends Environment {
 
     private Logger logger = Logger.getLogger("simulacaoCuidadorPervasivo."+Env.class.getName());
 
+
+    public String gerarTurno() {
+        Random gerador = new Random();
+        switch (gerador.nextInt(4)) {
+            case 0:
+                return "turno(manha)";
+            case 1:
+                return "turno(tarde)";
+            case 2:
+                return "turno(noite)";
+            case 3:
+                return "turno(madrugada)";        
+            default:
+                break;
+        }
+        return "";
+    }
+
+    public String gerarEventoExterno() {
+        Random gerador = new Random();
+        switch (gerador.nextInt(4)) {
+            case 0:
+                return "evento(chuva)";
+            case 1:
+                return "evento(barulho)";
+            case 2:
+                return "evento(visita)";
+            case 3:
+                return "evento(nenhum)";        
+            default:
+                break;
+        }
+        return "";
+    }
+
     /** Called before the MAS execution with the args informed in .mas2j */
     @Override
     public void init(String[] args) {
         super.init(args);
         try {
-            addPercept(ASSyntax.parseLiteral("turno(noite)"));
-            addPercept(ASSyntax.parseLiteral("chuva(on)"));
-            addPercept(ASSyntax.parseLiteral("tempestade(on)"));
+            addPercept(ASSyntax.parseLiteral(gerarTurno()));
+            addPercept(ASSyntax.parseLiteral(gerarEventoExterno()));
+
         } catch (ParseException e) {
             e.printStackTrace();
         }
